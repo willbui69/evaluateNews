558ed65542de591d82ed3c1865f6da45ae4439cd
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-10-25 12:43:23 +1100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -12,32 +12,5 @@ export {
     handleSubmit
    }
 
-// Send user data to the server
-document.getElementById('submit').addEventListener('click', sendData);
 
-function sendData() {
-    // Retrieve the text user want to check
-    const textInput = document.getElementById('name').value;
-
-    //Function to post data to server side
-    postData('/post',{text:textInput})   
-    .then(()=>
-        handleSubmit()
-    )
-}
-const postData = async (url = '', data= {}) => {
-    const response = await fetch(url, {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type":"application/json",
-        },
-        body: JSON.stringify(data),
-    })
-    try {
-        const newdata = await response.json();
-    } catch(error) {
-        console.log("error", error);
-    }
-}
   
