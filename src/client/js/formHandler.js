ccdd277b962e9cf44d4773432452b2f5ea18d667
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-10-25 12:42:07 +1100

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,13 +5,31 @@ async function handleSubmit(event) {
     let userInput = document.getElementById('name').value
     Client.checkForName(userInput)
 
+    postData('/post',{text:userInput}) 
+
     console.log("::: Form Submitted :::")
-    const res = await fetch('http://localhost:8081/get')
+    const res = await fetch('/get')
     try {
         const response = await res.json();
         document.getElementById('results').innerHTML = response.confidence
     }catch(error){
         console.log("error",error);
+    }
+}
+
+const postData = async (url = '', data= {}) => {
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(data),
+    })
+    try {
+        const newdata = await response.json();
+    } catch(error) {
+        console.log("error", error);
     }
 }
 
