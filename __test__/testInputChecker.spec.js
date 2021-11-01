9a8b54cfa26fb697c44caaf23bafac008bc8d056
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-11-01 15:34:16 +1100

    add service worker

--- __test__/testInputChecker.spec.js
+++ __test__/testInputChecker.spec.js
@@ -3,7 +3,9 @@ import { checkForInput } from "../src/client/js/inputChecker";
 
 describe("Testing the user input", () => {
     test("Testing the checkForInput() function", () => {
-           const input = 'abc';
-           expect(input).toBeTruthy();
+           const input1 = 'abc';
+           const input2 = '';
+           expect(input1).toBeTruthy();
+           expect(input2).not.toBeTruthy();
 
 })});
